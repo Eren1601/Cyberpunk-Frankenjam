fb6ac054d1629bf3f72ccfa23e16d0c95df393cd
Eren1601 <erentepe27@gmail.com>
2025-11-08 23:39:08 +0100

    Menu proto

---
+++ scenes/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var play_button: Button = $CenterContainer/VBoxContainer/Play
+@onready var quit_button: Button = $CenterContainer/VBoxContainer/Quit
+@onready var suprise_button: Button = $"CenterContainer/VBoxContainer/?"
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+	suprise_button.pressed.connect(_on_suprised_pressed)
+	
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/World.tscn")
+
+func _on_suprised_pressed() -> void:
+	OS.shell_open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
